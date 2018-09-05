00079d64c06d32b0fe8f079454ee1dcd7c9cf0ea
ziyouchutuwenwu <ziyouchutuwenwu@gmail.com>
2018-09-05 22:28:55 +0800

    添加验证器

--- src/main/java/com/mmc/springbootdemo/exception/BindExceptionHanlder.java
+++ src/main/java/com/mmc/springbootdemo/exception/BindExceptionHanlder.java
@@ -9,19 +9,23 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class BindExceptionHanlder {
 
     @ExceptionHandler(BindException.class)
-    public Result handleBindException(BindException ex) {
+    public Result handleBindException(BindException exception) {
 
-        // ex.getFieldError():随机返回一个对象属性的异常信息。如果要一次性返回所有对象属性异常信息，则调用ex.getAllErrors()
-        FieldError fieldError = ex.getFieldError();
+        // exception.getFieldError():随机返回一个对象属性的异常信息。如果要一次性返回所有对象属性异常信息，则调用 exception.getAllErrors()
+        FieldError fieldError = exception.getFieldError();
         StringBuilder stringBuilder = new StringBuilder();
 
-        stringBuilder.append(fieldError.getField()).append("=[").append(fieldError.getRejectedValue()).append("]")
-                .append(fieldError.getDefaultMessage());
+        stringBuilder.
+                append(fieldError.getField()).
+                append("=[").
+                append(fieldError.getRejectedValue()).
+                append("]").
+                append(fieldError.getDefaultMessage());
 
-        // 生成返回结果
         Result errorResult = new Result();
         errorResult.setCode(400);
         errorResult.setMessage(stringBuilder.toString());
+
         return errorResult;
     }
 }
